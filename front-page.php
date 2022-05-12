1cccf5f3e6471b39cc9082fd2a92c2973574996b
Ryan <rjamesbradshaw@gmail.com>
2022-05-12 16:43:36 -0700

    removed event template to template parts

--- front-page.php
+++ front-page.php
@@ -15,7 +15,7 @@
     <div class="full-width-split__inner">
         <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
 
-<!-- Custom Query -->
+<!-- Custom Query for EVENTS -->
         <?php
             $today = date('Ymd');
             $homePageEvents = new WP_Query(array(
@@ -35,32 +35,15 @@
             ));
 
             while($homePageEvents -> have_posts()){
-                $homePageEvents->the_post(); ?>
+                $homePageEvents->the_post();
+                get_template_part('template-parts/content', get_post_type());
+                //first arg is filepath, second is -xyz where xyz is the dash after the "content" as shown above (eg: content-event)
+                ?>
 
-                <div class="event-summary">
-                    <a class="event-summary__date t-center" href="#">
-                        <span class="event-summary__month"><?php
-                            $eventDate = new DateTime(get_field('event_date'));
-                            echo $eventDate -> format('M');
-                        ?></span>
-                        <span class="event-summary__day"><?php
-                            echo $eventDate -> format('d');
-                        ?></span>
-                    </a>
-                    <div class="event-summary__content">
-                        <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                        <p><?php if(has_excerpt()){
-                            echo get_the_excerpt();
-                        } else {
-                            echo wp_trim_words(get_the_content(), 20);
-                        }
-                        ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
-                    </div>
-                </div>
 
             <?php } wp_reset_postdata(); //ALWAYS USE THIS METHOD after using a CUSTOM query
         ?>
-<!-- end custom query -->
+<!-- end custom query Events-->
 
         <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event'); ?>" class="btn btn--blue">View All Events</a></p>
     </div>
