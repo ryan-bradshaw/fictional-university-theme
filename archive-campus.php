1e3d21782e6284a2dbc8788bdba6f469dbea7ea2
Ryan <rjamesbradshaw@gmail.com>
2022-05-12 15:49:16 -0700

    all banners updated except campuses

--- archive-campus.php
+++ archive-campus.php
@@ -1,7 +1,9 @@
 <?php
 get_header();
+// pageBanner(array(
+// ));
 ?>
-
+<!-- update to dynamic page banner! -->
 <div class="page-banner">
     <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg')?>);"></div>
         <div class="page-banner__content container container--narrow">
