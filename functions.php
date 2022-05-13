284fb1e295a820ed95583078d2cf0cd28ff405a9
Ryan <rjamesbradshaw@gmail.com>
2022-05-12 17:19:21 -0700

    minor formatting

--- functions.php
+++ functions.php
@@ -21,7 +21,7 @@ function pageBanner($args = NULL){
         <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['image']; ?>);"></div>
         <div class="page-banner__content container container--narrow">
             <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
-                <div class="page-banner__intro">
+        <div class="page-banner__intro">
                 <p><?php echo $args['subtitle']; ?></p>
         </div>
     </div>
