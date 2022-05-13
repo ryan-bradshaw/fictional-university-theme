f76f2c32975f96108d05433f6ca0478828e07003
Ryan <rjamesbradshaw@gmail.com>
2022-05-12 17:07:19 -0700

    minor formatting changes, campuses archive

--- archive-campus.php
+++ archive-campus.php
@@ -1,18 +1,10 @@
 <?php
 get_header();
-// pageBanner(array(
-// ));
+pageBanner(array(
+    'title' => 'Our Campuses',
+    'subtitle' => 'We have two beautiful campuses in the heart of Manhattan'
+));
 ?>
-<!-- update to dynamic page banner! -->
-<div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg')?>);"></div>
-        <div class="page-banner__content container container--narrow">
-            <h1 class="page-banner__title"> Our Campuses </h1>
-            <div class="page-banner__intro">
-                <p>Our campuses are located all over. Find us near you!</p>
-        </div>
-    </div>
-</div>
 
 <div class="container container--narrow page-section">
     <div class="acf-map">
